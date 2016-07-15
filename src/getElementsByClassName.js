61010bfefcb07ec901ddb0072aba2843385da4e4
Drew Kosta <drewkosta@gmail.com>
2016-07-14 18:58:16 -0700

    completed basic reqs

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+  var inner = function (node) {
+    if (node.classList.contains(className)) {
+      result.push(node);
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      inner(node.children[i]);
+    }
+  };
+  inner(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+  var inner = function (obj) {
+    if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null) {
+      result += obj;
+    } else if (typeof obj === 'string') {
+      result += '"' + obj + '"';
+    } else if (Array.isArray(obj)) {
+      result += '[';
+      for (var i = 0; i < obj.length; i++) {
+        inner(obj[i]);
+        result += ',';
+      }
+      if (result.charAt(result.length - 1) === ',') {
+        result = result.slice(0, -1);
+      }
+      result += ']';
+    } else if (typeof obj === 'object') {
+      result += '{';
+      for (var key in obj) {
+        if (typeof obj[key] !== 'function' && obj[key] !== undefined && typeof key !== 'function' && key !== 'undefined') {
+          result += '"' + key + '":';
+          inner(obj[key]);
+          result += ',';
+        }
+      }
+      if (result.charAt(result.length - 1) === ',') {
+        result = result.slice(0, -1);
+      }
+      result += '}';
+    }
+  };
+  inner(obj);
+  return result;
 };
